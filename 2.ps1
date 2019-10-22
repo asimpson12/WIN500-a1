9c425ddb8dddecf9896f9265993e30d857f0da36
asimpson12 <54990143+asimpson12@users.noreply.github.com>
2019-10-22 13:50:15 -0400

    Add files via upload

---
+++ 2.ps1
@@ -0,0 +1,5 @@
+﻿$domainPCS = Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
+Set-Content C:\Temp\down.txt $null
+$domainPCS | % {
+    if (!(Test-NetConnection -ComputerName $_ -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).PingSucceeded){Add-Content C:\Temp\down.txt "`n$_"}
+}
---
+++ QueryStorage+RAM.ps1
@@ -0,0 +1,7 @@
+﻿$storageInfo = New-Object psobject -Property @{
+    OS = Get-ComputerInfo -Property OsName | Select-Object -ExpandProperty OsName
+    TotalRAM = [math]::Round((([double](Get-ComputerInfo -Property OsTotalVisibleMemorySize | Select-Object -ExpandProperty OsTotalVisibleMemorySize) /1024)/1024), 2) #In GB
+    TotalHDD = [math]::Round(((([double](Get-PSDrive C | Select-Object -ExpandProperty Used) + [double](Get-PSDrive C | Select-Object -ExpandProperty Free))/1024)/1024), 2) #In MB
+    FreeHDD = [math]::Round((([double](Get-PSDrive C | Select-Object -ExpandProperty Free)/1024)/1024), 2) #In MB
+}
+$storageInfo
