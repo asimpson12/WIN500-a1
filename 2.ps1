7cba24954bd4441ca70810f93af24b4f7bdc3869
asimpson12 <54990143+asimpson12@users.noreply.github.com>
2019-11-01 12:05:36 -0400

    Add files via upload

--- 2.ps1
+++ 2.ps1
@@ -7,12 +7,15 @@ $domainPCS = Get-ADComputer -Filter * | Select-Object -ExpandProperty Name
 #Empty old contents of down list
 Set-Content C:\Temp\down.txt $null
 
+#Empty old contents of output webpage
+$null | Set-Content C:\Temp\SpaceInfo.html
+
 #Init list of storage info
 $storageQueries = @()
 
 $domainPCS | % {
     if (!(Test-NetConnection -ComputerName $_ -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).PingSucceeded){
-        Add-Content C:\Temp\down.txt "`n$_"
+        Add-Content C:\Temp\down.txt -Value "`n<p><b>$_</b></p>"
     } else {
         $pcName = $_
         try{$storageQueries += Invoke-Command -ComputerName $pcName -FilePath C:\Temp\QueryStorage+RAM.ps1 -ErrorAction Stop}
@@ -20,10 +23,24 @@ $domainPCS | % {
     }
 }
 
-$storageQueries | Select-Object -Property FreeHDD, TotalHDD, TotalRAM, OS | ft @{L='Disk Space Free (MB)';E={$_.FreeHDD}}, @{L='Total Disk Space (MB)';E={$_.TotalHDD}}, @{L='Total RAM (GB)';E={$_.TotalRAM}}, @{L='Operating System';E={$_.OS}} -AutoSize | sort -Property OS
+#Format info gathered from script invokations, store in output buffer
+$storageQueries | Select-Object  @{L='Disk Space Free (MB)';E={$_.FreeHDD}}, @{L='Total Disk Space (MB)';E={$_.TotalHDD}}, @{L='Total RAM (GB)';E={$_.TotalRAM}}, @{L='Operating System';E={$_.OS}} | ConvertTo-Html -Title "Query Results" -Body $_ | Out-File -FilePath C:\Temp\tmp.html
+
+$tmpPage = Get-Content C:\Temp\tmp.html
 
 #Print hosts that are down (if appropriate)
 if ($(gc C:\Temp\down.txt) -ne $null){
-    Write-Host "The following systems are down:"
-    gc C:\Temp\down.txt
-}
+    ForEach ($line in $tmpPage) {
+        Add-Content -Path C:\Temp\SpaceInfo.html $line
+        if ($line -like "</table>"){
+            Add-Content -Path C:\Temp\SpaceInfo.html -Value "<p></p>"
+            Add-Content -Path C:\Temp\SpaceInfo.html -Value "<p></p>"
+            Add-Content -Path C:\Temp\SpaceInfo.html -Value  "<p><u>The following systems are down:</u></p>"
+            gc C:\Temp\down.txt | Add-Content -Path C:\Temp\SpaceInfo.html
+        }
+    }
+}else{gc C:\Temp\tmp.html | Set-Content C:\Temp\SpaceInfo.html}
+
+ 
+
+Invoke-Item C:\Temp\SpaceInfo.html
